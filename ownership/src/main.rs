03b366805d7e8aa6124222c82f669f9ebd6c6abd
Rowan Kinneavy <rkinneavy@gmail.com>
2023-11-14 22:39:40 +0000

    extra note on slices vs string literals

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,4 @@
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -17,3 +17,12 @@ fn main() {
 
     println!("the first word is: {}", word);
 }
+
+/* 
+## Remember this!
+
+Recall that we talked about string literals being stored inside the binary. Now that we know about slices, we can properly understand string literals:
+
+let s = "Hello, world!";
+The type of s here is &str: it’s a slice pointing to that specific point of the binary. This is also why string literals are immutable; &str is an immutable reference.
+ */
