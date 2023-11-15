24cd128974ccb4c52986be6516d5c7d0eccca533
Rowan Kinneavy <rkinneavy@gmail.com>
2023-11-15 20:05:55 +0000

    first stabs at int-to-binary/hex converter n stuff

---
+++ num_conv/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let line = std::io::stdin().lines().next().unwrap().unwrap();
+    println!("{}", &line);
+}
+
+
+// I have no idea what I'm doing in this language but I want to write a recursive 'divide by 2 and store the remainder' 
+// solution. I guess you use closures for that. :shrug: WIP
+fn int_to_binary(u32: input_int) -> u32 {
+    mut result: u32 = 0;
+    mut Vector<u32>: binary_collector = 0;
+    b = &input_int;
+    
+    let while (b != 0) {
+        let collect_remainder = |x| x%2;
+
+
+
+
+    }  
+}
